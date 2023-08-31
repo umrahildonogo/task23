c05f637c074361da2e402507c5acc191061890bb
raccoon <donogo.umrahil@yandex.ru>
2023-08-31 17:51:47 +0300

    tablecube

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿void TabCube(int num)
+{
+    int count = 1;
+    while(count <= num)
+    {
+        Console.WriteLine($"{count, 3} -> {count*count*count, 5}");
+        count++;
+    }
+}
+
+Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+TabCube(number);
